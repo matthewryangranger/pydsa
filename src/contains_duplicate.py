49eae6b242a2fd2d6db42c591e12f20b79a71391
Matthew Ryan Granger <mattg1827@gmail.com>
2026-04-23 14:47:52 -0700

    . f added contains_duplicate test and implementation

--- src/contains_duplicate.py
+++ src/contains_duplicate.py
@@ -2,4 +2,9 @@
 ### (https://leetcode.com/problems/contains-duplicate/)
 
 def contains_duplicate(nums: list[int]) -> bool:
+    seen: set[int] = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
     return False
--- tests/test_contains_duplicate.py
+++ tests/test_contains_duplicate.py
@@ -8,3 +8,27 @@ def test_single_int_list():
     result: bool = contains_duplicate(single_int_list)
     # Then
     assert result is False
+
+def test_empty_list():
+    # Given
+    empty_list: list[int] = []
+    # When
+    result: bool = contains_duplicate(empty_list)
+    # Then
+    assert result is False
+
+def test_duplicate_list():
+    # Given
+    duplicate_list: list[int] = [1, 2, 3, 4, 1]
+    # When
+    result: bool = contains_duplicate(duplicate_list)
+    # Then
+    assert result is True
+
+def test_no_duplicate_list():
+    # Given
+    no_duplicate_list: list[int] = [1, 3, 2, 5]
+    # When
+    result: bool = contains_duplicate(no_duplicate_list)
+    # Then
+    assert result is False
